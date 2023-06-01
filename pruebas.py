0e8c942236b4e33b4a65dce0cc6a295c32c1aac3
alambrito <islerkain@gmail.com>
2023-05-31 22:03:50 -0400

    ultimas correcciones

--- pruebas.py
+++
@@ -1,7 +0,0 @@
-import random
-caracteres ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-contrasenia = ''
-for i in range(10):
-    contrasenia += random.choice(caracteres)
-
-print(contrasenia)
